3fd94c7b0e558dee564077664cca05524e9abb1a
Boudewijn van der Heide <boudewijn@delta-utec.com>
2026-05-19 18:18:14 +0200

    core: update setup_env patch to new upstream init diff
    Signed-off-by: Boudewijn van der Heide <boudewijn@delta-utec.com>

--- setup_env.sh
+++ setup_env.sh
@@ -12,5 +12,5 @@ ln -s 8192mib-ext4-es-current.img sda
 
 mkdir mnt
 mount -o loop sda mnt
-patch -p0 mnt/init < ../patches/0001-add-n-benchmarks-to-init.diff
+patch -p0 mnt/init < ../patches/0001-add-n-benchmarks-to-init-new-upstream.diff
 umount mnt
